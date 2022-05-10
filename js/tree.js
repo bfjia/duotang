a199764cc01f5c548ec1dd79b72ef7089f6ac43a
Art Poon <artpoon@gmail.com>
2022-05-09 23:37:44 -0400

    draw tree in rectangular layout; colour branches by PANGO group

--- js/tree.js
+++ js/tree.js
@@ -1,21 +1,27 @@
 /**
  *  Based on drawtree.js from https://github.com/PoonLab/CoVizu
  * 
- *  r2d3 predefines `div`, `height` and `weight` variables
+ *  r2d3 predefines `data`, `div`, `height` and `weight` variables
+ *  data is an Object with two entries, `nodes` and `edges`
+ *  `edges` is an Array with entries:
+ *   {parent: 9429, child: 9430, length: 0.0095, isTip: false, x0: 0, …}
+ *  `nodes` is an Array with entries:
+ *   {label: 'hCoV-19/Canada/...', n.tips: 0, x: 0.2423, y: 1}
  */
 
 console = d3.window(div.node()).console;
+console.log(data);
 
 var svg = div.append("svg")
              .attr("width", width+"px")
-             .attr("height", heigiht+"px");
+             .attr("height", height+"px");
 
 var margin = {top: 0, right: 50, bottom: 20, left: 50},
     width = width - margin.left - margin.right,
     height = height - margin.top - margin.bottom,
     g = svg.append("g")
-           .attr("height", plotheight+"px")
-           .attr("id", "barplot-group")
+           .attr("height", height+"px")
+           .attr("id", "treeplot-group")
            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // append tooltip element
@@ -28,6 +34,21 @@ var tooltip = div.append("div")
     .style("pointer-events", "none");
 
 // set up plotting scales
-var xScale = d3.scaleLinear().range([0, width]),
-    yScale = d3.scaleLinear().range([height, 40]);
+var xmax = d3.max(data.edges, e => e.x1),
+    ntips = data.nodes.filter(x => x['n.tips'] == 0).length,
+    xScale = d3.scaleLinear().domain([0, xmax]).range([0, width]),
+    yScale = d3.scaleLinear().domain([0, ntips]).range([height, 40]);
+
+var treeplot = g.selectAll("lines")
+                .data(data.edges)
+                .enter().append("line")
+                .attr("class", "lines")
+                .attr("x1", function(d) { return xScale(d.x0); })
+                .attr("x2", function(d) { return xScale(d.x1); })
+                .attr("y1", function(d) { return yScale(d.y0); })
+                .attr("y2", function(d) { return yScale(d.y1); })
+                .attr("stroke-width", 1.0)
+                .attr("stroke", function(d) { return d.colour; });
+
+
 
