815d37a1fb69c6a2c2132b729670f082c74fedf5
bfjia <bfjia@bfjia.net>
2023-01-05 14:23:21 -0800

    test2

--- scripts/getPastDuotangVersions.sh
+++ scripts/getPastDuotangVersions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#simple check to make sure the script is ran at root of main branch
+#simple checks to make sure the script is ran at root of main branch
 if test -f "duotang.html"; then
     echo "check:current dir is repo root. - YES"
 else
@@ -27,28 +27,25 @@ paste - - -d, < commitHistory.txt > commitHistory2.txt
 sed -i '/data_needed/d' commitHistory2.txt
 
 mkdir -p archive
-echo "Here we store old versions of the duotang notebook:\n" > archive/readme.md
+echo "Here we store old versions of the duotang notebook:" > archive/readme.md
+echo "<ul>" >> archive/readme.md
 #recreate the duotang.html file from each commit and save it
 for i in `cat commitHistory2.txt`; do
 	echo $i;
 	name=`echo $i | cut -d',' -f2`
 	commit=`echo $i | cut -d',' -f1`
-	echo $commit
-	echo $name
 	id=`echo $commit | cut -d'.' -f2`
 	date=`echo $commit | cut -d'.' -f1`
-	echo $id
-	echo $date
 	git show $id:$name > archive/$commit.html
 	if [ -s archive/$commit.html ]; then
         # echo the hyperlink to the readme
-        echo "[$date](/archive/$commit.html)\n" >> archive/readme.md
+        echo "<li>[$date]($commit.html)</li>" >> archive/readme.md
 	else
 		# The file is empty, sometimes the symlink for follow pulls up random files not related to duotang,html, just delete it.
 		rm -f archive/$commit.html
 	fi
 done;
-
-find archive/ -type f -empty -print -delete
+#cleanup
+echo "</ul>" >> archive/readme.md
 rm commitHistory.txt
 rm commitHistory2.txt
